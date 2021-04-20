7d419051f3705a099479ad2bb4fd5688febe3569
liubowolkova <10179875+liubowolkova@users.noreply.github.com>
2021-04-20 17:21:34 +0300

    Task 2. UIImageView bug fixed. Every cell in the table contains colored icon-image and named label

--- Module_10/Task2ViewController.swift
+++ Module_10/Task2ViewController.swift
@@ -7,14 +7,48 @@
 
 import UIKit
 
+struct SettingsItem {
+    var name: String
+    var icon: String
+    var color: UIColor
+}
+
 class Task2ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     private lazy var names = ["Авиарежим", "Wi-Fi", "Bluetooth", "Сотовая связь", "Режим модема", "Уведомления", "Звуки, тактильные сигналы", "Не беспокоить", "Экранное время", "Основные", "Пункт управления", "Экран и яркость"]
+    private lazy var colors: [UIColor] = [.orange, .blue, .blue, .green, .green, .red, .systemPink, .purple, .purple, .gray, .gray, .blue]
+    private lazy var icons = ["airplane", "wifi", "link", "antenna.radiowaves.left.and.right", "personalhotspot", "square", "volume.3.fill", "moon.fill", "hourglass", "gear","capsule", "textformat.size"]
+    private lazy var settingsItemList: [SettingsItem] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        settingsItemList = self.generateSettingsItemList(names: names, icons: icons, colors: colors)
+        
         // Cut extra cells
         tableView.tableFooterView = UIView()
+    }
+    
+    private func setSystemIconSettings(imageView: UIImageView, icon: String, color: UIColor) {
+        imageView.image = UIImage(systemName: icon)
+        imageView.backgroundColor = color
+        imageView.layer.cornerRadius = 5
+        imageView.tintColor = .white
+    }
+    
+    private func createSettingsItem(name: String, icon: String, color: UIColor) -> SettingsItem {
+        let item = SettingsItem(name: name, icon: icon, color: color)
+        
+        return item
+    }
+    
+    private func generateSettingsItemList(names: [String], icons: [String], colors: [UIColor]) -> [SettingsItem]{
+        var array: [SettingsItem] = []
+        for (i, e) in names.enumerated() {
+            array.append(self.createSettingsItem(name: e, icon: icons[i], color: colors[i]))
+        }
+        
+        return array
     }
 }
 
@@ -25,8 +59,10 @@ extension Task2ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsCell") as! Task2TableViewCell
-        // cell.imageContainer.image = UIImage(named: "Pic_1.png")!
-        cell.label.text = names[indexPath.row]
+        let currentItem = settingsItemList[indexPath.row]
+        
+        self.setSystemIconSettings(imageView: cell.imageContainer, icon: currentItem.icon, color: currentItem.color)
+        cell.label.text = currentItem.name
         
         return cell
     }
